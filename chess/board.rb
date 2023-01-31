dfb3a22ef9305008d1733b97084cd93ee62b1950
Steven Notovitz <sjn39@cornell.edu>
2023-01-31 17:07:42 -0500

    Update board.rb

--- chess/board.rb
+++ chess/board.rb
@@ -16,13 +16,22 @@ class Board
         grid[pos[0]][pos[1]]
     end
 
+    def move_piece(start_pos, end_pos)
+        if self[start_pos] == nil
+            raise "there is no piece at #{start_pos}"
+        elsif end_pos[0] < 0 || end_pos[0] > 7 || end_pos[1] < 0 || end_pos[1] > 7
+            raise "the piece cannot move to #{end_pos}"
+        else
+            self[end_pos] = self[start_pos]
+            self[start_pos] = nil
+            # grid[end_pos[0]][end_pos[1]] = grid[start_pos[0]][start_pos[1]]
+            # grid[start_pos[0]][start_pos[1]] = nil
+        end
+    end
+
+    private
     def []=(pos, value)
         grid[pos[0]][pos[1]] = value
     end
 
-    def move_piece(start_pos, end_pos)
-        if self[start_pos] == nil
-            raise "there is no piece at start pos"
-        end
-    end
 end
