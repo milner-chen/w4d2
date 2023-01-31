9b38a6035f11b79b22eb93d523fa6491e39cc5d8
Steven Notovitz <sjn39@cornell.edu>
2023-01-31 17:58:33 -0500

    Update class_inheritance.rb

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -18,7 +18,7 @@ class Manager < Employee
     attr_reader :employees
 
     def initialize(name, title, salary, boss = nil)
-        super(name, title, salary, boss)
+        super
         @employees = []
     end
 
