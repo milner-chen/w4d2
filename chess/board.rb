7f3819bd52492498480aafe574440cb72247c577
Steven Notovitz <sjn39@cornell.edu>
2023-02-01 16:50:57 -0500

    Populate board

--- chess/board.rb
+++ chess/board.rb
@@ -9,22 +9,33 @@ require_relative 'pieces/pawn'
 class Board
 
     def initialize
-        @grid = Array.new(8) { Array.new(8) }
-        [0, 1].each do |i|
-            @grid[i].each_index do |j|
-                @grid[i][j] = King.new(:black, self, [i, j])
-            end
+        @grid = Array.new(8) { Array.new(8, NullPiece.instance) }
+
+        @grid[1].each_index do |i|
+            @grid[1][i] = Pawn.new(:black, self, [1, i])
         end
-        (2..5).each do |i|
-            @grid[i].each_index do |j|
-                @grid[i][j] = NullPiece.instance
-            end
+
+        @grid[0][0] = Rook.new(:black, self, [0, 0])
+        @grid[0][1] = Knight.new(:black, self, [0, 1])
+        @grid[0][2] = Bishop.new(:black, self, [0, 2])
+        @grid[0][3] = Queen.new(:black, self, [0, 3])
+        @grid[0][4] = King.new(:black, self, [0, 4])
+        @grid[0][5] = Bishop.new(:black, self, [0, 5])
+        @grid[0][6] = Knight.new(:black, self, [0, 6])
+        @grid[0][7] = Rook.new(:white, self, [0, 7])
+        
+        @grid[6].each_index do |i|
+            @grid[6][i] = Pawn.new(:white, self, [6, i])
         end
-        [6, 7].each do |i|
-            @grid[i].each_index do |j|
-                @grid[i][j] = Queen.new(:white, self, [i, j])
-            end
-        end
+
+        @grid[7][0] = Rook.new(:white, self, [7, 0])
+        @grid[7][1] = Knight.new(:white, self, [7, 1])
+        @grid[7][2] = Bishop.new(:white, self, [7, 2])
+        @grid[7][3] = Queen.new(:white, self, [7, 3])
+        @grid[7][4] = King.new(:white, self, [7, 4])
+        @grid[7][5] = Bishop.new(:white, self, [7, 5])
+        @grid[7][6] = Knight.new(:white, self, [7, 6])
+        @grid[7][7] = Rook.new(:white, self, [7, 7])
     end
 
     def move_piece(start_pos, end_pos)
@@ -38,7 +49,6 @@ class Board
         end
     end
 
-    # private
     attr_reader :grid
 
     def [](pos)
